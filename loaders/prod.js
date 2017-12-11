007bd1161943145ca777289e486a694d24fa0d70
Justin Ball <justinball@gmail.com>
2017-12-11 15:33:28 -0700

    Bring loaders into project. Adds dev release process

---
+++ loaders/prod.js
@@ -0,0 +1,3 @@
+var atomicSearchWidgetScript = document.createElement("script");
+atomicSearchWidgetScript.src = "https://d2u53n8918fnto.cloudfront.net/atomic_search_widget.js" + "?ts=" + new Date().getTime();
+document.getElementsByTagName("head")[0].appendChild(atomicSearchWidgetScript);
