64215a5160392966da579c7a521b871508cd5786
Ryker Blunck <rykerblunck@gmail.com>
2019-12-18 11:03:19 -0700

    update documentation to reflect new setup

---
+++ loaders/local.js
@@ -0,0 +1,4 @@
+var atomicSearchWidgetScript = document.createElement("script");
+atomicSearchWidgetScript.src = "https://atomic-search-widget.atomicjolt.xyz/atomic_search_widget.js";
+document.getElementsByTagName("head")[0].appendChild(atomicSearchWidgetScript);
+
