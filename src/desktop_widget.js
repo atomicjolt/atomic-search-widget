b651d1da1f22a928fcf843bacc8d665d1f4b1b37
Ryker Blunck <ryker.blunck@atomicjolt.com>
2023-02-15 17:41:08 -0700

    remove duplicate type="submit" and move both to the beginning

--- src/desktop_widget.js
+++ src/desktop_widget.js
@@ -13,7 +13,7 @@ function html(cssClass, placeholder) {
       <form id="ajas-search-form" class="ajas-search-widget__form" action="javascript:void(0);" method="get" role="search">
         <label for="ajas-search01" class="ajas-search-widget-hidden">Search</label>
         <input type="text" placeholder="${placeholder}" id="ajas-search01" />
-        <button type="submit" aria-label="submit search" class="ajas-search-widget__btn--search" type="submit">
+        <button type="submit" aria-label="submit search" class="ajas-search-widget__btn--search">
           ${SEARCH_SVG}
         </button>
         ${dropdownHtml}
